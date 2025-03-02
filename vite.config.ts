29a0dadea857568dc381829306a4f1b7b54a47e7
Sriramreddy <sriramreddy42@gmail.com>
2025-03-02 01:21:44 -0500

    build issues fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// filepath: /C:/Programming/Projects/fetch-dogs/vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000, // Adjust the chunk size warning limit if needed
+  },
+  server: {
+    open: true,
+  },
+});
